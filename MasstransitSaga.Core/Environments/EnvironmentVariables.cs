d8a7483a33bffc17eb7c574993abb36e6a543d71
Lê Anh Toàn <toananhle91@gmail.com>
2025-01-09 14:04:03 +0700

    Update EnvironmentVariables.cs

--- MasstransitSaga.Core/Environments/EnvironmentVariables.cs
+++ MasstransitSaga.Core/Environments/EnvironmentVariables.cs
@@ -6,4 +6,8 @@ public static class EnvironmentVariables
 {
     public const string PostgresConnectionString = "POSTGRES_CONNECTION_STRING";
     public static bool HasPostgresConnectionString() => !string.IsNullOrEmpty(Environment.GetEnvironmentVariable(PostgresConnectionString));
+    public const string MySQLConnectionString = "MYSQL_CONNECTION_STRING";
+    public static bool HasMySQLConnectionString() => !string.IsNullOrEmpty(Environment.GetEnvironmentVariable(MySQLConnectionString));
+    public const string SQLServerConnectionString = "SQLSERVER_CONNECTION_STRING";
+    public static bool HasSQLServerConnectionString() => !string.IsNullOrEmpty(Environment.GetEnvironmentVariable(SQLServerConnectionString));
 }
