676f92c7e04f90e3cc9940c6c9f39e1e29a3faeb
Lê Anh Toàn <toananhle91@gmail.com>
2025-01-09 14:13:23 +0700

    Update ServiceExtensions.cs

--- MasstransitReactApp.Server/Extensions/ServiceExtensions.cs
+++ MasstransitReactApp.Server/Extensions/ServiceExtensions.cs
@@ -4,72 +4,72 @@ namespace MasstransitReactApp.Server.Extensions;
 
 public static class ServiceExtensions
 {
-  public static void AddSqlServerIdentityInfrastructure(this IServiceCollection services, string assembly)
-  {
-      var sp = services.BuildServiceProvider();
-      using (var scope = sp.CreateScope())
-      {
-          var _dbSetting = scope.ServiceProvider.GetRequiredService<IDatabaseSettingsProvider>();
-          string appConnStr = _dbSetting.GetSQLServerConnectionString();
-          services.AddDbContext<IdentityContext>(options =>
-              options.UseSqlServer(appConnStr,
-              b =>
-              {
-                  b.MigrationsAssembly(assembly);
-                  // b.MigrationsHistoryTable("__EFMigrationsHistory", "onion");
-              }
-          )
-          );
-      }
-  }
-
-  public static void AddMySqlIdentityInfrastructure(this IServiceCollection services, string assembly)
-  {
-      // Build the intermediate service provider
-      var sp = services.BuildServiceProvider();
-      using (var scope = sp.CreateScope())
-      {
-          var _dbSetting = scope.ServiceProvider.GetRequiredService<IDatabaseSettingsProvider>();
-          string appConnStr = _dbSetting.GetMySQLConnectionString();
-          if (!string.IsNullOrWhiteSpace(appConnStr))
-          {
-              var serverVersion = new MySqlServerVersion(new Version(5, 7, 35));
-              services.AddDbContext<IdentityContext>(options =>
-              options.UseMySql(
-                  appConnStr, serverVersion,
-                  b =>
-                  {
-                      b.SchemaBehavior(MySqlSchemaBehavior.Ignore);
-                      b.EnableRetryOnFailure(
-                          maxRetryCount: 5,
-                          maxRetryDelay: TimeSpan.FromSeconds(30),
-                          errorNumbersToAdd: null);
-                      b.MigrationsAssembly(assembly);
-                      b.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
-                  }));
-          }
-      }
-  }
-
-  public static void AddNpgSqlIdentityInfrastructure(this IServiceCollection services, string assembly)
-  {
-      // Build the intermediate service provider
-      var sp = services.BuildServiceProvider();
-      using (var scope = sp.CreateScope())
-      {
-          var _dbSetting = scope.ServiceProvider.GetRequiredService<IDatabaseSettingsProvider>();
-          string appConnStr = _dbSetting.GetPostgresConnectionString();
-          if (!string.IsNullOrWhiteSpace(appConnStr))
-          {
-              services.AddDbContext<IdentityContext>(options =>
-              options.UseNpgsql(
-              appConnStr,
-              b =>
-              {
-                  b.MigrationsAssembly(assembly);
-                  b.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
-              }));
-          }
-      }
-  }
+  public static void AddSqlServerPersistenceInfrastructure(this IServiceCollection services, string assembly)
+    {
+        var sp = services.BuildServiceProvider();
+        using (var scope = sp.CreateScope())
+        {
+            var _dbSetting = scope.ServiceProvider.GetRequiredService<IDatabaseSettingsProvider>();
+            string appConnStr = _dbSetting.GetSQLServerConnectionString();
+            services.AddDbContext<ApplicationDbContext>((serviceProvider, options) =>
+            options
+            .AddInterceptors(serviceProvider.GetRequiredService<StatusUpdateInterceptor>())
+            .UseSqlServer(
+            appConnStr,
+            b => b.MigrationsAssembly(assembly)
+            ));
+        }
+    }
+  
+    public static void AddMySqlPersistenceInfrastructure(this IServiceCollection services, string assembly)
+    {
+        // Build the intermediate service provider
+        var sp = services.BuildServiceProvider();
+        using (var scope = sp.CreateScope())
+        {
+            var _dbSetting = scope.ServiceProvider.GetRequiredService<IDatabaseSettingsProvider>();
+            string appConnStr = _dbSetting.GetMySQLConnectionString();
+            if (!string.IsNullOrWhiteSpace(appConnStr))
+            {
+                var serverVersion = new MySqlServerVersion(new Version(5, 7, 35));
+                services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseMySql(
+                    appConnStr, serverVersion,
+                    b =>
+                    {
+                        b.SchemaBehavior(MySqlSchemaBehavior.Ignore);
+                        b.EnableRetryOnFailure(
+                            maxRetryCount: 5,
+                            maxRetryDelay: TimeSpan.FromSeconds(30),
+                            errorNumbersToAdd: null);
+                        b.MigrationsAssembly(assembly);
+                        b.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
+                    }));
+            }
+        }
+    }
+  
+    public static void AddNpgSqlPersistenceInfrastructure(this IServiceCollection services, string assembly)
+    {
+        // Build the intermediate service provider
+        var sp = services.BuildServiceProvider();
+        using (var scope = sp.CreateScope())
+        {
+            var _dbSetting = scope.ServiceProvider.GetRequiredService<IDatabaseSettingsProvider>();
+            string appConnStr = _dbSetting.GetPostgresConnectionString();
+            if (!string.IsNullOrWhiteSpace(appConnStr))
+            {
+                services.AddDbContext<ApplicationDbContext>((options) =>
+                options.UseNpgsql(
+                appConnStr,
+                b =>
+                {
+                    b.MigrationsAssembly(assembly);
+                    b.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
+                })
+  
+                );
+            }
+        }
+    }
 }
